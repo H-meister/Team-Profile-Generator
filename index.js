1361d5004c69a73f93c12956ac2b62d927003361
H-meister <Hector1626hg@gmail.com>
2022-09-19 12:58:09 -0400

    adding questions for the TeamManager

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 //start on getting questions starting with inquirer and fs
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const teamManager = [
+    {
+        type : 'input',
+        name : 'teamManager',
+        message: 'What is your Team Manager name?'
+    }
+    {
+        type : 'input',
+        name : 'email',
+        message : 'What is their e-mail?'
+    }
+    {
+        
+    }
+]
 //then prompt questions to the user and get the results back
 //then generate Html page to get information displayed 
 //lastly run test programs i think usiing another npm thingy
