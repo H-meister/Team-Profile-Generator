c8c20e63d7999da8a4a2e5a8b53402433688431c
H-meister <Hector1626hg@gmail.com>
2022-09-19 12:52:48 -0400

    more file things to get started

---
+++ index.js
@@ -0,0 +1,4 @@
+//start on getting questions starting with inquirer and fs
+//then prompt questions to the user and get the results back
+//then generate Html page to get information displayed 
+//lastly run test programs i think usiing another npm thingy
---
+++ src/page-generator.js
@@ -0,0 +1,1 @@
+//This is where I will generate the HTML
