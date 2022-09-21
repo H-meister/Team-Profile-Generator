a882eed38e5c68c06fed27c95125623a23bf7416
H-meister <Hector1626hg@gmail.com>
2022-09-21 01:14:49 -0400

    adding more functionality in the user prompts then soon adding way to generate html

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //start on getting questions starting with inquirer and fs
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateHTML = require('./src/page-generator');
 //make a teamManager objectarray
 const teamManager = [
     {
@@ -30,12 +31,119 @@ const teamManager = [
         choices : ['Yes', 'No']
     }
 ];
+
+const yes = [
+    {
+        type : 'list',
+        name :'yes',
+        message : 'What would you like to do?',
+        choices : ['Add Engineer','Add Intern','Finish building team!']
+    }
+]
+//adding engineer to project --will test to see if it works
+const engineer = [
+    {
+        type : 'input',
+        name : 'enName',
+        message : 'What is the engineers name?'
+    },
+    {
+        type : 'input',
+        name : 'enID',
+        message : 'What is the engineers ID?'
+    },
+    {
+        type : 'input',
+        name : 'enEmail',
+        message : 'What is the engineers E-mail?'
+    },
+    {
+        type : 'input',
+        name : 'enGit',
+        message : 'What is the engineers Github username?'
+    }
+];
+//after finishing here you are to be taken back to the menu to see if you want to add
+//an intern.
+//also adding intern here with relevent questions.
+const intern = [
+    {
+        type : 'input',
+        name : 'inName',
+        message : 'What is the interns name?',
+    },
+    {
+        type : 'input',
+        name : 'inID',
+        message : 'What is the interns ID?',
+    },
+    {
+        type : 'input',
+        name : 'inEmail',
+        message : 'What is the interns E-mail?'
+    },
+    {
+        type : 'input',
+        name : 'inSchool',
+        message : 'Where did the intern go to school?'
+    }
+];
+
+function writeHTML () {
+    writeToFile('./index.HTML', generateHTML(answers));  
+};
+
+//function to run the questions
 function init (){
 inquirer
-.prompt(teamManager);
+//prompting initial teamManager array to prompt user
+.prompt(teamManager)
+//with the answers i am then checking what they selected to continue
+.then(function (answers){
+    // console.log(answers);
+    //in this senario if they do agree then the application continues and prompts yes
+    if(answers.choice === 'Yes'){
+        inquirer
+        .prompt(yes)
+        .then(function (answers) {
+            //depending on who they choose we will select what to do
+            console.log(answers);
+            if (answers.yes === 'Add Engineer'){
+                inquirer
+                .prompt(engineer)
+                .then(function (answers){
+                    inquirer
+                    .prompt(yes);
+                })
+            }
+            else if(answers.yes === 'Add Intern'){
+                inquirer
+                .prompt(intern)
+            }
+            //here return to just finish page. ( which is writing new HTML)
+            else {
+                writeHTML(answers);
+            }
+        })
+    }
+    //add function for if they choose no (just build the app at this point)
+    else {
+        writeHTML(answers);
+    }
+});
 };
 
 init();
-//then prompt questions to the user and get the results back
 //then generate Html page to get information displayed 
 //lastly run test programs i think usiing another npm thingy
+
+//TODO
+//currently answers work but running into mirror effect where I will have to keep
+//going and going
+
+//create CALL BACK functions for the YES prompt 
+//then prompt the yes callback
+
+//figure out why index.html file is not being created at the writeHTML function!
+
+//then figure out jEST :()
--- src/page-generator.js
+++ src/page-generator.js
@@ -1,1 +1,19 @@
 //This is where I will generate the HTML
+function generateHTML (answers)  {
+return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NEW THING</title>
+</head>
+<body>
+    <h1>Webpage Created!</h1>
+</body>
+</html>
+`
+};
+
+module.exports = generateHTML;
